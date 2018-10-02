bdadc0582f6e54ec45926d337e1d65c30cbd03c1
Lewis Cruz <bluelew75@gmail.com>
2018-10-02 19:49:44 -0400

    Added controllers and models.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/no-assets'
+  get 'post/index'
+  get 'post/show'
+  get 'post/no-assets'
   get 'pages/post'
   get 'pages/search'
   get 'pages/search_results'
